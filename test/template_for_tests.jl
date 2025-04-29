d88c3fab548fb20f67368bc5cd108a056e2d4a06
Ritter.Marc <ritter.marc@physik.uni-muenchen.de>
2025-04-29 10:42:52 +0200

    added template for unit tests

---
+++ test/template_for_tests.jl
@@ -0,0 +1,12 @@
+using Test
+#import tn_julia: somefunction
+# If you need more imports, put them here.
+
+@testset "name of testset" begin
+    # Code to be run, e.g. initialization of some tensors.
+    # @test some expression == another expression
+    # Also valid: !=, >, <, >=, <=, ≈
+    # See https://docs.julialang.org/en/v1/stdlib/Test/#Test.@test
+end
+
+# Put more testsets here.
