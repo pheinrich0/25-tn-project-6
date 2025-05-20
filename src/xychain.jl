19d85190d42b095faef6394cde67b35285a8942c
Ritter.Marc <ritter.marc@physik.uni-muenchen.de>
2025-05-20 10:33:10 +0200

    added extendhamiltonian() function

--- src/xychain.jl
+++ src/xychain.jl
@@ -1,25 +1,56 @@
-
 function xypairhamiltonian()
     Splus, Sminus, Sz, Id = spinlocalspace(1 // 2)
     A = identity(Splus, 1, Splus, 1)
 
-    SplusSplus = updateLeft(
-        reshape(Splus, (2, 1, 2)),
-        A,
-        reshape(Splus, (1, 2, 1, 2)),
-        A
+    # add a third leg that sums over + and -
+    Splusminus = cat(Splus, Sminus, dims=3)
+    # leg ordering for left tensor in updateLeft
+    Sleft = permutedims(Splusminus, (1, 3, 2))
+    # leg ordering for right tensor in updateLeft
+    Sright = reshape(
+        permutedims(Splusminus, (3, 1, 2)),
+        (2, 2, 1, 2)
     )
 
-    SminusSminus = updateLeft(
-        reshape(Sminus, (2, 1, 2)),
-        A,
-        reshape(Sminus, (1, 2, 1, 2)),
-        A
-    )
+    StimesS = updateLeft(Sleft, A, Sright, A)
 
-    Hxy = reshape(
-        -(SplusSplus + SminusSminus),
-        (4, 4)
-    )
+    # remove the dummy leg that was needed for updateLeft
+    Hxy = reshape(-StimesS, (4, 4))
     return Hxy
+end
+
+function extendhamiltonian(H_ell::AbstractMatrix, A_ell::AbstractArray{<:Number, 3})
+    Dleft = size(A_ell, 1)
+    Ileft = reshape(I(Dleft), (Dleft, 1, Dleft))
+
+    Splus, Sminus, Sz, Id = spinlocalspace(1 // 2)
+    A_ellplus1 = identity(A_ell, 3, Splus, 1)
+
+    d = size(Sz, 1)
+
+    Splusminus = cat(Splus, Sminus, dims=3)
+    # leg ordering for right tensor in first updateLeft
+    Sright1 = reshape(
+        permutedims(Splusminus, (1, 3, 2)),
+        (1, 2, 2, 2)
+    )
+    S1 = updateLeft(Ileft, A_ell, Sright1, A_ell)
+
+    # leg ordering for right tensor in second updateLeft
+    Sright2 = reshape(
+        permutedims(Splusminus, (3, 1, 2)),
+        (2, 2, 1, 2)
+    )
+    S2 = updateLeft(S1, A_ellplus1, Sright2, A_ellplus1)
+
+    D = size(H_ell, 1)
+    H2 = updateLeft(
+        reshape(H_ell, (D, 1, D)),
+        A_ellplus1,
+        reshape(I(2), (1, 2, 1, 2)),
+        A_ellplus1
+    )
+
+    H_ellplus1 = reshape(H2 - S2, (d * D, d * D))
+    return H_ellplus1, A_ellplus1
 end
