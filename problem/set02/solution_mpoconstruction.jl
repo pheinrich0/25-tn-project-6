d945c520a810de1bfc670e584a6ec02c8ce0e223
Ritter.Marc <ritter.marc@physik.uni-muenchen.de>
2025-05-21 12:36:15 +0200

    added some comments to solution_mpoconstruction

--- problem/set02/solution_mpoconstruction.jl
+++ problem/set02/solution_mpoconstruction.jl
@@ -1,15 +1,19 @@
 using LinearAlgebra
 import tn_julia: xychainmpo, iterativediagonalization, extendhamiltonian_xy, updateLeft
 
+# part (i): Construct the MPO; see src/xychain.jl
 J = 1.0
 L = 50
 mpo = xychainmpo(L, J)
 
+# part (ii) (1): Compute the ground state using iterative diagonalization
 Nkeep = 300
 extendhamiltonian(H::AbstractMatrix, A::AbstractArray{<:Number, 3}) = extendhamiltonian_xy(H, A; J=J)
 energies, mps = iterativediagonalization(extendhamiltonian, 2, L, Nkeep, truncationtolerance=0.0)
 println("Ground state energy from iterative diagonalization: $(energies[end])")
 
+# part (ii) (2): Obtain the energy expectationvalue <Psi|H|Psi> using the MPO, and compare
+# with the energy obtained from iterative diagonalization
 function expectationvalue(
     mps::AbstractVector{<:AbstractArray{<:Number, 3}},
     mpo::AbstractVector{<:AbstractArray{<:Number, 4}},
